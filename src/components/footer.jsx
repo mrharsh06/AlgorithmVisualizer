1166c3f84a2b53f0f08301e01c450a9eaf44c903
mrharsh06 <mrharsh0606@gmail.com>
2023-01-24 23:25:11 +0530

    Algorithm Visualizer

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created with <i className="fa fa-heart" style={{color: "#FF5252"}}></i> by
+                <a href="https://www.linkedin.com/in/harsh-chaudhary-563b47199/" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                    Harsh Chaudhary
+                </a>
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
